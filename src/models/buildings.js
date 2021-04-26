28c69540f0232d48a24055995d69af6669597840
lynds0408 <81365397+lynds0408@users.noreply.github.com>
2021-04-26 22:53:19 +0800

    buildings and rooms model was created

---
+++ src/models/buildings.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class buildings extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The models/index file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  buildings.init(
+    {
+      buildingID: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      buildingName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg : "Building Name is required"},
+          notEmpty: { msg : 'Building Name should not be empty'},
+        },
+        unique: {msg: "The Building Name is already existed."},
+      },
+
+      
+  }, {
+    sequelize,
+    timestamps: true,
+    createdAt: "addedAt",
+    modelName: 'buildings',
+  });
+  return buildings;
+};
---
+++ src/models/rooms.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class rooms extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  rooms.init(
+    {
+    roomID: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    }, 
+    roomName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg : "Room Name is required"},
+        notEmpty: { msg : 'Room Name should not be empty'},
+      },
+      unique: {msg: "The Room Name is already existed."},
+    }
+  }, {
+    sequelize,
+    timestamps: true,
+    createdAt: 'addedAt',
+    modelName: 'rooms',
+  });
+  return rooms;
+};
