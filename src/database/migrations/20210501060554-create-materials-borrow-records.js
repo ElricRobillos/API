4d9c364dde22489d55d22bb52eb6e81fdd9d0f61
Elric Robillos <robilloselric@gmail.com>
2021-05-01 14:14:30 +0800

    Created MaterialsBorrowRecords model

---
+++ src/database/migrations/20210501060554-create-materials-borrow-records.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('materials_borrow_records', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('materials_borrow_records');
+  }
+};
---
+++ src/models/materials_borrow_records.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class materials_borrow_records extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  materials_borrow_records.init({
+    borrowID : {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    }, 
+    dueDate : {
+      type : DataTypes.DATE
+    },
+    returnDate: {
+      type : DataTypes.DATE
+    },
+    returnProcessBy : {
+      type: DataTypes.STRING
+    },
+    proofOfReturn : {
+      type : DataTypes.STRING
+    },
+    status :{
+      type: DataTypes.STRING,
+      validate : {
+        isIn:[["Not Return Yet", "Returned",  "Overdue"]] 
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'materials_borrow_records',
+  });
+  return materials_borrow_records;
+};
