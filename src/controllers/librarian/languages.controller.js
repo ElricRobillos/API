abc13dcdccb462492975f6a099ab3f5ae4d0ea32
Vanessah Buenaventura <68227233+Bandroite@users.noreply.github.com>
2021-07-22 19:04:05 +0800

    update genres and languages controller

--- src/controllers/librarian/languages.controller.js
+++ src/controllers/librarian/languages.controller.js
@@ -51,7 +51,44 @@ exports.find_language = (req, res) => {
   else{
     const id = req.params.languageID;
 
-    languages.findByPk(id)
+    languages.findByPk(id,{
+      include: [
+          {
+              model: db.users,
+              as: "added_by_librarian",
+              attributes:{
+                  exclude: [
+                      'password',
+                      'profilePic',
+                      'section',
+                      'course',
+                      'year',
+                      'addedBy',
+                      'updatedBy',
+                      'addedAt',
+                      'updatedAt'
+                  ]
+              }
+          },
+          {
+              model: db.users,
+              as: "updated_by_librarian",
+              attributes:{
+                  exclude: [
+                      'password',
+                      'profilePic',
+                      'section',
+                      'course',
+                      'year',
+                      'addedBy',
+                      'updatedBy',
+                      'addedAt',
+                      'updatedAt'
+                  ]
+              }
+          }
+      ]
+  })
     .then((data) => dataResponse(res, data, process.env.SUCCESS_RETRIEVED, process.env.NO_DATA_RETRIEVED))
     .catch((err)  => errResponse(res, err));
   }
